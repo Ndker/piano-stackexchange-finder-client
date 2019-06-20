eef0153239fc5adad38641eaf27afbb078f52369
Ann Teneneva <a.teneneva@taxi-master.com>
2019-06-20 19:35:00 +0400

    * post and get requests for backend; * input and button for search text; * display results of searching;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {SearchService} from 'src/app/service/search.service';
+import {ISearchResultItem, SearchService} from 'src/app/service/search.service';
 
 @Component({
   selector: 'app-root',
@@ -8,50 +8,47 @@ import {SearchService} from 'src/app/service/search.service';
 })
 export class AppComponent {
   title = 'stackexchange-finder-client';
-  data = [
-    {
-      date: '2019-06-19T08:07:07.000Z',
-      title: 'How to fix OutOfMemory on Android',
-      author: 'masi mojib',
-      sourceLink: 'https://stackoverflow.com/questions/56662903/how-to-fix-outofmemory-on-android'
-    },
-    {
-      date: '2019-06-05T04:45:13.000Z',
-      title: 'Netty NioEventLoopGroup causes OutOfmemory in Vertx 3.x',
-      author: 'Barcelona',
-      sourceLink: 'https://stackoverflow.com/questions/56454371/netty-nioeventloopgroup-causes-outofmemory-in-vertx-3-x'
-    },
-    {
-      date: '2019-06-17T12:40:49.000Z',
-      title: '.NET OutOfMemory exception - Reason: Low on memory during GC',
-      author: 'Tony Sepia',
-      sourceLink: 'https://stackoverflow.com/questions/56631535/net-outofmemory-exception-reason-low-on-memory-during-gc'
-    },
-    {
-      date: '2019-06-12T16:04:04.000Z',
-      title: 'OutofMemory exception while building docker images asp.net core',
-      author: 'Zain Malik',
-      sourceLink: 'https://stackoverflow.com/questions/56566323/outofmemory-exception-while-building-docker-images-asp-net-core'
-    }
-  ];
+  // data = [
+  //   {
+  //     date: '2019-06-19T08:07:07.000Z',
+  //     title: 'How to fix OutOfMemory on Android',
+  //     author: 'masi mojib',
+  //     sourceLink: 'https://stackoverflow.com/questions/56662903/how-to-fix-outofmemory-on-android'
+  //   },
+  //   {
+  //     date: '2019-06-05T04:45:13.000Z',
+  //     title: 'Netty NioEventLoopGroup causes OutOfmemory in Vertx 3.x',
+  //     author: 'Barcelona',
+  //     sourceLink: 'https://stackoverflow.com/questions/56454371/netty-nioeventloopgroup-causes-outofmemory-in-vertx-3-x'
+  //   },
+  //   {
+  //     date: '2019-06-17T12:40:49.000Z',
+  //     title: '.NET OutOfMemory exception - Reason: Low on memory during GC',
+  //     author: 'Tony Sepia',
+  //     sourceLink: 'https://stackoverflow.com/questions/56631535/net-outofmemory-exception-reason-low-on-memory-during-gc'
+  //   },
+  //   {
+  //     date: '2019-06-12T16:04:04.000Z',
+  //     title: 'OutofMemory exception while building docker images asp.net core',
+  //     author: 'Zain Malik',
+  //     sourceLink: 'https://stackoverflow.com/questions/56566323/outofmemory-exception-while-building-docker-images-asp-net-core'
+  //   }
+  // ];
 
   private loading: boolean;
   private page: number;
   private size: number;
+  private total: number;
   private hasNext: boolean;
   private requestId: string;
+  private data: ISearchResultItem[];
 
   constructor(private searchService: SearchService) {
-    this.loading = true;
+    this.loading = false;
     this.page = 1;
     this.hasNext = false;
     this.size = 10;
-  }
-
-  search(text: string) {
-    this.loading = true;
-    this.page = 1;
-    this.requestId = this.searchService.startSearch(text);
+    this.total = -1;
   }
 
   prev() {
@@ -59,7 +56,7 @@ export class AppComponent {
       return;
     }
 
-    this.page++;
+    this.page--;
     this.getData();
   }
 
@@ -68,7 +65,7 @@ export class AppComponent {
       return;
     }
 
-    this.page--;
+    this.page++;
     this.getData();
   }
 
@@ -84,9 +81,30 @@ export class AppComponent {
     return this.data;
   }
 
+  search(text: string) {
+    this.loading = true;
+    this.page = 1;
+    this.searchService.startSearch(text).subscribe((result) => {
+      this.requestId = result.requestId;
+      this.getData();
+    });
+  }
+
   private getData() {
     this.loading = true;
-    this.searchService.getResult(this.requestId, this.page, this.size);
+
+    this.searchService.getResult(this.requestId, this.page, this.size).subscribe((result) => {
+      if (result.status === 202) {
+        setTimeout(() => this.getData(), 1000);
+      } else if (result.status === 200) {
+        this.hasNext = result.body.meta.hashNext;
+        this.page = result.body.meta.currentPage;
+        this.total = result.body.meta.totalPages;
+        this.data = result.body.data;
+
+        this.loading = false;
+      }
+    });
   }
 
 }
--- src/app/service/search.service.ts
+++ src/app/service/search.service.ts
@@ -1,23 +1,50 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class SearchService {
-
-  // TODO: вынести в настройки  (environment?)
-  private url = 'http://127.0.0.1:8081';
 
   constructor(private http: HttpClient) {
   }
 
-  startSearch(text: string): string {
-    return '';
+  startSearch(text: string) {
+    const params = new HttpParams()
+      .set('text', text);
+
+    return this.http.post<IStartResult>(`${environment.url}/api/search`, null, {params: params});
   }
 
   getResult(requestId: string, page: number, size: number) {
-  }
+    const params = new HttpParams()
+      .set('requestId', requestId)
+      .set('page', `${page}`)
+      .set('size', `${size}`);
 
+    return this.http.get<ISearchResult>(`${environment.url}/api/search/result`, {observe: 'response', params: params});
+  }
+}
+
+export interface IStartResult {
+  requestId: string;
+}
+
+export interface ISearchResult {
+  data: ISearchResultItem[];
+  meta: ISearchResultMeta;
+}
+
+export interface ISearchResultMeta {
+  currentPage: number;
+  hashNext: boolean;
+  totalPages: number;
+}
+
+export interface ISearchResultItem {
+  date: string;
+  title: string;
+  author: string;
+  sourceLink: string;
 }
